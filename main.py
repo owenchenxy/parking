1c25d7b36e2ba2aa49b3b10a992192c13da69c58
Xianyao Chen <owenchenxy@gmail.com>
2023-03-13 18:27:50 +0800

    done

--- main.py
+++ main.py
@@ -8,11 +8,5 @@ def main():
     for pod in all_pods:
         migrate_pod_to_node(pod, find_target_node(pod))
 
-# migrate_pod_to_node("busybox1", "kind-worker")
-# migrate_pod_to_node("busybox2", "kind-worker")
-# migrate_pod_to_node("busybox3", "kind-worker")
-# set_node_space("kind-worker", 0)
-# set_node_space("kind-worker2", 2)
-# set_node_space("kind-worker3", 2)
 if __name__ == "__main__":
     main()
